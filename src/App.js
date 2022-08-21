d838a2d28cfb41d1adfdbc6cfb0c93a6b7b55051
Nazariy Dumanskyy <nazariy1995@gmail.com>
2022-08-20 19:24:14 -0700

    ADD: minting functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
 import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
+import { useMoralis } from "react-moralis";
+import abi from "./contracts/contract.json";
 
 function App() {
+  // create a state varaible for supply
+  const [totalSupply, setTotalSupply] = useState(0);
+  const { Moralis, enableWeb3, authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
+
+  const logOut = async () => {
+    await logout();
+  }
+
+  useEffect(()=>{
+    const getSupply = async () => {
+      if(isAuthenticated){
+        await enableWeb3();
+        getTotalSupply();
+      }
+    }
+    getSupply();
+  }, [isAuthenticated])
+
+  const mint = async () => {
+    const sendOptions = {
+      contractAddress: "0x718fD0990c355C315fcF5e00130D6f236a31d3E2",
+      functionName: "safeMint",
+      abi: abi,
+      msgValue: Moralis.Units.ETH("0.01")
+    };
+    // add ether to in sendOption for MOralis
+    const transaction = await Moralis.executeFunction(sendOptions);
+
+  }
+
+  // Create a function to get supply of our NFTs
+  // how many NFTs have been minted so far
+  // use the totalSupply function to get totalSupply()
+
+  const getTotalSupply = async () =>{
+    const sendOptions = {
+      contractAddress: "0x718fD0990c355C315fcF5e00130D6f236a31d3E2",
+      functionName: "totalSupply",
+      abi: abi
+    };
+    // add ether to in sendOption for MOralis
+    const message = await Moralis.executeFunction(sendOptions);
+    setTotalSupply(message.toNumber());
+  }
+
+  const login = async () => {
+    if (!isAuthenticated) {
+      await authenticate({signingMessage: "Log in using Moralis" })
+        .then(function (user) {
+          console.log("logged in user:", user);
+          console.log(user.get("ethAddress"));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video className="background-video" width="400" height="400" autoPlay muted playsInline loop>
+        <source src="https://cdn-std.droplr.net/files/acc_990213/aIieHW" type="video/mp4" />
+      </video>
+      <div className='main'>
+        <div className='minting'>
+          <div className='left-column'>
+            <video width="400" height="400" autoPlay muted playsInline loop>
+              <source src="https://cdn-std.droplr.net/files/acc_990213/d9ucir" type="video/mp4" />
+            </video>
+          </div>
+          <div className='right-column'>
+            <h2>YOOOO: INTO THE METAVERSE</h2>
+            <div>{totalSupply} minted / 200</div>
+            <div className='actions'>
+              {
+                isAuthenticated
+                  ? <div className='mintStart'>
+                      <div onClick={mint} className='wallet'>MINT</div> 
+                      <div onClick={logOut} className='wallet'>START OVER</div> 
+                    </div>
+                  : <div onClick={login} className='wallet'>CONNECT WALLET</div> 
+              }
+
+            </div>
+          </div>
+        </div>
+        <div className='footer'>
+           MINTING NOW
+        </div>
+      </div>
     </div>
   );
 }
